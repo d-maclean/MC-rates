33572bfe8fd485fb0c9b1417e5a60ddaeab453d5
d-maclean <dmaclean@unc.edu>
2025-04-17 15:43:56 -0400

    update imports

--- MC_rates/__init__.py
+++ MC_rates/__init__.py
@@ -5,8 +5,8 @@ sys.path.append(
     os.path.split(os.path.abspath(__file__))[0]
     )
 
-from rates_classes import BinariesBin, MCSampler
-from rates_functions import calc_SFR_madau_fragos, calc_mean_metallicity_madau_fragos
+from rates import *
+from rates_functions import *
 
 if __name__ == "__main__":
     
